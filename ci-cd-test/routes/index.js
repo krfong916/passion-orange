88ad662d2bc383acf1cab0053d78db2952094a19
Kyle Fong <krfong@ucsc.edu>
2020-03-14 21:56:59 -0700

    remove ci/cd test

--- ci-cd-test/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const routes = express.Router();
-
-routes.get('/resources', (req, res, next) => res.send('fetched resources'));
-routes.get('/healthy', (req, res, next) => res.send(''));
-routes.get('/', (req, res, next) => res.send(process.env.VAR_TEST));
-
-export default routes;
